41f3b942074a2f17c0183df54315f536bba4362a
Iwona Piotrowska <iwona.piotrowska9@gmail.com>
2017-01-30 23:06:13 +0100

    added routing using ui-router

--- app/app.js
+++ app/app.js
@@ -1,1 +1,23 @@
-angular.module('app', ['ui.router']);
+angular.module('app', ['ui.router'])
+	.config(['$stateProvider', "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+			.state('home', {
+				url: "/home",
+				templateUrl: "app/components/home/_home.html",
+				controller: "HomeCtrl"
+			})
+			.state('question', {
+				url: "/question/:id",
+				templateUrl: "app/components/single-question/_single-question.html",
+				controller: "SingleQuestionCtrl"
+			})
+			.state('user-profile', {
+				url: "/user-profile/:id",
+				templateUrl: "app/components/user-profile/_user-profile.html",
+				controller: "UserProfileCtrl"
+			});
+
+		$urlRouterProvider.otherwise('/home');
+
+	}]);
