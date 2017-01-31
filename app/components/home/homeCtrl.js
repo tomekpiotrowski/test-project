58c5e0c3141f8babe29f30d2efba04283b5c887c
Iwona Piotrowska <iwona.piotrowska9@gmail.com>
2017-01-31 16:20:20 +0100

    created search system

--- app/components/home/homeCtrl.js
+++ app/components/home/homeCtrl.js
@@ -2,26 +2,31 @@ angular.module('app')
 	.controller('HomeCtrl', ['$scope', 'Users', 'Questions', '$window', '$rootScope',
 		function($scope, Users, Questions, $window, $rootScope) {
 
-		$scope.questions = Questions;
+			$scope.questions = Questions;
 
-		angular.element($window).bind('resize', function() {
-			$scope.$apply(function() {
-				$scope.documentWidth = $window.innerWidth;
+			angular.element($window).bind('resize', function() {
+				$scope.$apply(function() {
+					$scope.documentWidth = $window.innerWidth;
+				});
 			});
-		});
 
-		$scope.$watch('documentWidth', function(newValue) {
-			if (newValue <= 767) {
-				$scope.displayedColNum = 0;
-			} else if (newValue >= 768 && newValue <= 991) {
-				$scope.displayedColNum = 0;
-			} else if (newValue >= 992) {
-				$scope.displayedColNum = 3;
-			}
-		});
+			$scope.$watch('documentWidth', function(newValue) {
+				if (newValue <= 767) {
+					$scope.displayedColNum = 0;
+				} else if (newValue >= 768 && newValue <= 991) {
+					$scope.displayedColNum = 0;
+				} else if (newValue >= 992) {
+					$scope.displayedColNum = 3;
+				}
+			});
 
-		$scope.getUser = function(index) {
-			$rootScope.userIndex = index;
-		};
+			$scope.getUser = function(index) {
+				$rootScope.userIndex = index;
+			};
 
-	}]);
+			//search function
+			$scope.searchQuery = function() {
+				$scope.searchResult = $scope.searchInput || "";
+			};
+
+		}]);
